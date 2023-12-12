8d43bda194212a5373e3bcc6c7f5ee364fceaae5
Karolina Popławska <karolinaapoplawska@gmail.com>
2023-12-13 00:24:37 +0100

    Update index.js
    yy

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,32 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    var fs = require('fs');
-
-var htmlContent = '<html>Whatever</html>';
-
-fs.writeFile('/my-page.html', htmlContent, (error) => { /* handle error */ });
-
-  )
+class HelloWorld extends React.Component {
+  render() {
+    return <p>Hello, world!</p>;
+  }
 }
+
+// export default function Home() {
+//   return (
+    
+//     <div className="container">
+//       <Head>
+//         <title>Next.js Starter!</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <Header title="Welcome to my app!" />
+//         <p className="description">
+//           Get started by editing <code>pages/index.js</code>
+//         </p>
+//         <p className="description">
+//           TEST
+//         </p>
+//       </main>
+
+//       <Footer />
+//     </div>
+//   )
+// }
